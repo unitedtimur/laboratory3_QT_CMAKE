70f251cb4d3be5c682c8d9926c87c828e08332bb
Timur Likhomanov <lihomanov.t@gmail.com>
2020-06-02 19:59:44 +0700

    added charts

--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,11 @@ int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
 
-	// Проверка на утечку памяти
-#ifdef _DEBUG
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-	
-	// Инициализируем управляющий класс и передаём туда стратегию для группировки по папкам
-	//StrategyManagement* strategyManagement = new StrategyManagement(new FolderGrouping);
-	//// Выполнение бизнес-логики
-	//strategyManagement->doIt(QDir::currentPath(), QList<Data>());
-	//// Выставление новое стратегии для группировки по типам
-	//strategyManagement->setStrategy(new TypeGrouping);
-	//// Выполнение бизнес-логики
-	//strategyManagement->doIt(QDir::currentPath(), QList<Data>());
-	//
-	//// Очищение памяти
-	//delete strategyManagement;
-	
-	// Запускаются тесты
-	// ConfugurationTest configurationTest;
-	// FolderGroupingTest folderGroupingTest; 
-	// TypeGroupingTest typeGrupingTest;
-
 	// Окно главной формы
-	Explorer explorer;
-	explorer.show();
+    Explorer explorer;
+    explorer.show();
 
-	//QApplication::exec();
-	a.exec();
-
+	QApplication::exec();
+	
 	return 0;
 }
